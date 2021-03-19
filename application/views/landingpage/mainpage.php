23164c77f1076b0e15be8a0ecbe71f26c0b36a38
Reynan Jardin <yungga321@gmail.com>
2021-03-19 15:52:30 +0800

    updated

--- application/views/landingpage/mainpage.php
+++ application/views/landingpage/mainpage.php
@@ -11,15 +11,15 @@
             <div class="row h-100">
                 <div class="col-12 my-auto">
                     <div class="masthead-content text-white py-5 py-md-0">
-                        <h1 class="mb-3">Pasima-Taloy Elementary School</h1>
+                        <h1 class="mb-3">PTES - RCTW</h1>
                         <p class="mb-5">Here's what we've got for you!
                             <strong>Select what best suits for you!</strong></p>
 
-                        <a href="http://192.168.0.165:8181/59ff649f-4a49-b188-e050-f370617cca47/A/User:The_other_Kiwix_guy/Landing" target="_blank"><img src="<?php echo base_url('assets/png/wikilogo.png') ?>" class="img-fluid rounded" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.100.87:8181/59ff649f-4a49-b188-e050-f370617cca47/A/User:The_other_Kiwix_guy/Landing" target="_blank"><img src="<?php echo base_url('assets/png/wikilogo.png') ?>" class="img-fluid rounded" alt="" style="width: 70%; height: 70%"></a>
 
-                        <a href="http://192.168.0.188:8080/" target="_blank"><img src="<?php echo base_url('assets/png/learnershub.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.100.87:8080/" target="_blank"><img src="<?php echo base_url('assets/png/learnershub.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
 
-                        <a href="#"><img src="<?php echo base_url('assets/png/kolibri.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.100.87:33853/" target="_blank"><img src="<?php echo base_url('assets/png/kolibri.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
                         
                     </div>
                 </div>
