d794445108da474af34b549a05e0aee944bed665
yungga123 <53594401+yungga123@users.noreply.github.com>
2021-03-29 10:09:25 +0800

    UPDAT

--- application/views/landingpage/mainpage.php
+++ application/views/landingpage/mainpage.php
@@ -16,11 +16,11 @@
                         <h1 class="mb-3 mt-4">PTES - RCTW</h1>
                         <p class="mb-5">A project of <b>Rotary Club of Taguig West and Vinculum Technologies Corp.</b> in partnership with Pasima-Taloy Elementary School and local barangay of Pasima and Taloy</p>
 
-                        <a href="http://192.168.100.70:8181/59ff649f-4a49-b188-e050-f370617cca47/A/User:The_other_Kiwix_guy/Landing" target="_blank"><img src="<?php echo base_url('assets/png/wikilogo.png') ?>" class="img-fluid rounded" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.0.11:8181/59ff649f-4a49-b188-e050-f370617cca47/A/User:The_other_Kiwix_guy/Landing" target="_blank"><img src="<?php echo base_url('assets/png/wikilogo.png') ?>" class="img-fluid rounded" alt="" style="width: 70%; height: 70%"></a>
 
-                        <a href="http://192.168.100.87:8080/" target="_blank"><img src="<?php echo base_url('assets/png/learnershub.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.0.10:280/Pasima-Taloy%20Elementary%20School/" target="_blank"><img src="<?php echo base_url('assets/png/learnershub.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
 
-                        <a href="http://192.168.100.87:37081/" target="_blank"><img src="<?php echo base_url('assets/png/kolibri.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
+                        <a href="http://192.168.0.10:8080/" target="_blank"><img src="<?php echo base_url('assets/png/kolibri.png') ?>" class="img-fluid rounded mt-4" alt="" style="width: 70%; height: 70%"></a>
                     </div>
                 </div>
             </div>
